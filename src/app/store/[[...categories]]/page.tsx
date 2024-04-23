7b1d730b97caa2d898061fcd1123758e71167969
EduardoMartinez <alberto@uxbilink.com>
2024-04-23 15:15:51 -0600

    solucion a category

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -9,19 +9,21 @@ interface CategoryProps {
   params: {
     categories: string[];
   };
-  searchParamas?: string;
+  searchParams?: string;
 }
 
-export default async function Categories(props: CategoryProps) {
+export default async function Category(props: CategoryProps) {
   const { categories } = props.params;
   let products = [];
   const collections = await getCollections();
-  
-  if(categories?.length > 0){
-    const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id;
-    products = await getCollectionProducts(selectedCollectionId)
+
+  if (categories?.length > 0) {
+    const selectedCollectionId = collections.find(
+      (collection: any) => collection.handle === categories[0]
+    ).id;
+    products = await getCollectionProducts(selectedCollectionId);
   } else {
-    products = await getProducts()
+    products = await getProducts();
   }
 
   return <ProductsWrapper products={products} />;
